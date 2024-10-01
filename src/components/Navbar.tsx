66bd04894bcfcac0ed1aeb6d4fc9eb030eb29072
Peter <49101240+peterphann@users.noreply.github.com>
2024-10-01 07:20:35 -0500

    Add company logos

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,9 @@
 import { Link, useLocation } from "react-router-dom";
 import logo from "../assets/logo.png"
 import { motion } from "framer-motion"
+import instagram from "../assets/instagram.png"
+import linkedin from "../assets/linkedin.png"
+import github from "../assets/github.png"
 
 interface Link {
   name: string;
@@ -18,17 +21,37 @@ function Navbar() {
   ]
 
   return (
-    <div className="w-screen flex bg-[#111111] p-2 fixed top-0 text-white">
-      <Link className="ml-8" to="/home">
-        <motion.div className="hover:cursor-pointer" whileHover={{ scale: 1.1}} whileTap={{ scale: 0.9 }}>
-          <img className="w-16" src={logo} alt="" draggable={false}/>
-        </motion.div>
-      </Link>
+    <div className="w-screen h-20 flex justify-between bg-[#111111] p-2 fixed top-0 text-white shadow-md">
+      <div className="flex">
+        <Link className="ml-8" to="/">
+          <motion.div className="hover:cursor-pointer" whileHover={{ scale: 1.1}} whileTap={{ scale: 0.9 }}>
+            <img className="max-w-16" src={logo} alt="" draggable={false}/>
+          </motion.div>
+        </Link>
 
-      <div className="flex flex-start items-center ml-20">
-        {links.map(link => (
-          <Link key={link.name} to={link.reference}>{link.name}</Link>
-        ))}
+        <div className="flex flex-start justify-between items-center ml-20">
+          {links.map(link => (
+            <Link className={`mx-10 ${location.pathname === link.reference && "bg-gradient-to-r from-koifish-white to-koifish-red text-transparent bg-clip-text"}`} key={link.name} to={link.reference}>
+              <motion.div whileHover={{ translateY: -2 }}>
+                {link.name}
+              </motion.div>
+            </Link>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex flex-start justify-between items-center ml-20 px-8">
+        <Link className="mx-4" to="https://github.com/peterphann">
+          <motion.img whileHover={{ scale: 1.1}} whileTap={{ scale: 0.9 }} src={github} className="w-7" alt="" />
+        </Link>
+
+        <Link className="mx-4" to="https://www.linkedin.com/in/peterlphan/">
+          <motion.img whileHover={{ scale: 1.1}} whileTap={{ scale: 0.9 }} src={linkedin} className="w-7" alt="" />
+        </Link>
+
+        <Link className="mx-4" to="https://www.instagram.com/peterrdeter/">
+          <motion.img whileHover={{ scale: 1.1}} whileTap={{ scale: 0.9 }} src={instagram} className="w-7" alt="" />
+        </Link>
 
       </div>
     </div>
