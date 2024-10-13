9d9c8ae6d67f8e6fee38ad39607e1726054b96b6
Peter Phan <pet3r.ph4n@gmail.com>
2024-10-13 17:31:16 -0500

    Install shadcn/ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        'koifish-white': '#f9ecd9',
-        'koifish-red': '#f75640',
-        'koifish-black': '#2f3451'
-      }
-    },
+  	extend: {
+  		colors: {
+  			'koifish-white': '#f9ecd9',
+  			'koifish-red': '#f75640',
+  			'koifish-black': '#2f3451'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
